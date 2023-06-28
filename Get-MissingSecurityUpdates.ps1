ac6badd8d936a36b7bd2b225f0dda47f2e23a9ee
BetaHydri <jan_tiedemann@hotmail.com>
2023-06-28 11:23:46 +0200

    Download Cab message updates

--- Get-MissingSecurityUpdates.ps1
+++ Get-MissingSecurityUpdates.ps1
@@ -613,10 +613,10 @@ function Get-WsusCab {
                 $totalBytes = $Global:Data.SourceArgs.TotalBytesToReceive
                 $receivedBytes = $Global:Data.SourceArgs.BytesReceived
                 If ($null -ne $percent) {
-                    Write-Progress -Activity ("Downloading {0} from {1} to {2}" -f $CabFile, $Uri, $CabPath) -Status ("{0} bytes \ {1} bytes" -f $receivedBytes, $totalBytes) -PercentComplete $percent
+                    Write-Progress -Activity ("Downloading {0} from {1}" -f $CabPath, $Uri) -Status ("`n{0} bytes \ {1} bytes" -f $receivedBytes, $totalBytes) -PercentComplete $percent
                 }
             }
-            Write-Progress -Activity ("Downloading {0} from {1} to {2}" -f $CabFile, $Uri, $CabPath) -Status ("{0} bytes \ {1} bytes" -f $receivedBytes, $totalBytes) -Completed
+            Write-Progress -Activity ("Downloading {0} from {1}" -f $CabPath, $Uri) -Status ("`n{0} bytes \ {1} bytes" -f $receivedBytes, $totalBytes) -Completed
         }
         catch [System.Net.WebException], [System.IO.IOException] {
             Write-Host "Unable to download $CabFile from $Uri"
